a6055a3e93b2774192251aa6ad3999e5f6f5fe19
Christopher Finster <finsternavy@gmail.com>
2022-05-23 22:47:44 -0500

    108_assignment_1
    calculator with python

---
+++ calculator.py
@@ -0,0 +1,100 @@
+# Calculator.py
+# Extreme calculator
+# Christopher Finster
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+def divide(num1, num2):
+    if num2 != 0:
+        result = num1 / num2
+        return result
+    print("Sorry! Can't divide by zero!")
+
+# function to print the header each time
+def menu():
+    print("="*100)
+    print("Extreme Calculator\nby Chris")
+    print("="*100)
+    print("[1] Sum\n[2] Subtract\n[3] Multiply\n[4] Divide\n[5] Exit")
+
+# Make sure the user is entering a number
+def enterValidNum():
+    num = (input("Enter a number: "))
+    try: 
+        val = int(num)
+        print("Input is an integer. Number = ", num)
+        return int(num)
+    except ValueError:
+        try: 
+            val = float(num)
+            print("Input is a float. Number = ", num)
+            return float(num)   
+        except ValueError:
+            print("Input was a string = ", num) 
+            calculate()
+
+def calculate():
+    menu()
+    option = (input("Enter the number of the operation you wish to perform: "))
+    print(f"[{option}]")
+
+    result = 0
+
+    if option == '1':
+        print("SUM")
+        num1 = enterValidNum()
+        num2 = enterValidNum()
+        result = add(num1, num2)
+        print(f"{num1} + {num2} = {result}")
+        
+    elif option == '2':
+        print("SUBTRACT")
+        num1 = enterValidNum()
+        num2 = enterValidNum()
+        result = subtract(num1, num2)
+        print(f"{num1} - {num2} = {result}")
+
+    elif option == '3':
+        print("MULTIPLY")
+        num1 = enterValidNum()
+        num2 = enterValidNum()
+        result = multiply(num1, num2)
+        print(f"{num1} * {num2} = {result}")
+
+    elif option == '4':
+        print("DIVIDE")
+        num1 = enterValidNum()
+        num2 = enterValidNum()
+        result = divide(num1, num2)
+        print(f"{num1} / {num2} = {result}")
+
+    elif option == '5':
+        print("Exiting now...\n\n\n")
+        exit()
+
+    else:
+        print("Invalid entry.  Try again!\n\n\n")
+        calculate()
+
+    again = input("New calculation? Type 'yes' or anything else to exit:")
+
+    if again.upper() == 'YES':
+        print("\n\n\n")
+        calculate()
+
+    else:
+        print("Exiting now...\n\n\n")
+        exit()
+    
+    
+calculate()
---
+++ test.py
@@ -0,0 +1,28 @@
+#This is a comment in python
+
+#This is a variable in python
+name = "Chris"
+last = 'Finster'
+age = 34
+weight = 168.5
+happy = True #False #Boolean values capital letter at start
+something = [5, 10, 3, 11, 1, 56, 23, 7]
+print("Hello World... again...")
+
+for num in something: print(num + 5)
+
+print(something)
+
+print(age+1)
+print(name*3) #repeats strings
+
+print(name + ' ' + last)
+
+print(f"{name} {last}")
+print(name + "\n" + last)
+
+def say_hello(): 
+    print('Hello')
+    print('A second line')
+
+say_hello()
